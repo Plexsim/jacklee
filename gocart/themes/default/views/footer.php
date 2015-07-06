7cd0af44dfe6b7fb5c187d529b1b5fa160db3031
KC Tan <kctan@tmt.my>
2015-07-07 00:32:58 +0800

    first commit jacklee

---
+++ gocart/themes/default/views/footer.php
@@ -0,0 +1,28 @@
+<!--scripts-->		
+<?php echo theme_js('jquery-1.10.1.min.js', true);?>	
+<?php echo theme_js('jquery.validate.min.js', true);?>
+
+
+<?php echo theme_js('jquery.tabify.js', true);?>
+
+<?php echo theme_js('jquery.swipebox.js', true);?>
+<?php echo theme_js('jquery.fitvids.js', true);?>
+<?php echo theme_js('twitter/jquery.tweet.js', true);?>
+<?php echo theme_js('email.js', true);?>
+
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+   <?php echo theme_js('bootstrap.js', true);?>        
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script>
+    
+    <?php echo theme_js('gmap3.js', true);?>
+    <?php echo theme_js('jquery.colorbox.js', true);?>
+    <?php echo theme_js('jquery.easing.js', true);?>
+    <?php echo theme_js('script.js', true);?>
+    <!-- Demo -->
+         
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>	
+</body>
+</html>
---
+++ gocart/themes/default/views/homepage.php
@@ -0,0 +1,328 @@
+<body data-spy="scroll" data-target=".navbar-default">
+
+  	<!-- begin:navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand visible-xs" href="#home">Jack Lee & Chloe Fong</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#home">Home</a></li>
+          	<li><a href="#about">About</a></li>
+          	<li><a href="#ceremony">Ceremony</a></li>
+          	<li><a href="#photos">Photos</a></li>
+          	<li><a href="#story">Story</a></li>
+          	<li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div>
+    </nav>
+    <!-- end:navbar -->
+
+    <!-- begin:home -->
+    <section id="home" style="background: url(<?php echo theme_img('img01.jpg') ?>);">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-md-12">
+    				<h1>Jack Lee & Chloe Fong</h1>
+					<h3><span>Are getting</span></h3>
+					<!-- <div class="line"><h3>Are getting</h3></div> -->
+					<h2><i class="fa fa-heart-o"></i> MARRIED! <i class="fa fa-heart-o"></i></h2>
+					<h4>And They are please to </h4>
+					<h5><span>~ Invite you ~</span></h5>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+    <!-- end:home -->
+	
+	<!-- begin:about -->
+	<section id="about">
+		<div class="about-inner">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<h2>Jack Lee</h2>
+						<p>Jack Lee, 21 Tahun who people called him Datuk Lee</p>
+						<blockquote>
+							Huat a ~
+							<small>Jack Lee</small>
+						</blockquote>
+					</div>
+					<!-- break -->
+
+					<div class="col-md-4">
+						<div class="about-img-container" style="background: url(<?php echo theme_img('img05.jpg')?> center;"></div>
+						<img src="<?php echo theme_img('bottom.png') ?>" class="about-img img-responsive">
+					</div>
+					<!-- break -->
+
+					<div class="col-md-4">
+						<h2>Chloe Fong</h2>
+						<p>Chloe Fong, 21 Tahun. Wanita yang murah senyum, tinggi, langsing.
+						Hobi ialah suka teman dengan Jack Lee.</p>
+						<blockquote>
+							Ong a ~
+							<small>Chloe Fong</small>
+						</blockquote>
+					</div>
+					<!-- break -->
+
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end:about -->
+
+	<!-- begin:ceremony -->
+	<section id="ceremony" style="background: url(<?php echo theme_img('img04.jpg') ?>);">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1>The Ceremony</h1>
+					<h3><span>Will Be Held On</span></h3>
+					<h2>09 - 09 - 2015</h2>
+					<h5><span>~ September ~</span></h5>
+					<h4>Dynasty Dragon Seafood Restaurant</h4>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end:ceremony -->
+
+	<!-- begin:photos -->
+	<section id="photos">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<img src="<?php echo theme_img('bottom.png') ?>" class="about-img img-responsive">
+					<h2>Wedding Gallery</h2>
+					<p>Show Time</p>
+				</div>
+				<!-- break -->
+
+				<div class="col-md-8">
+					<div id="wedding-photo" class="carousel slide" data-ride="carousel">
+				      <div class="carousel-inner">
+				        <div class="item">
+				          <div class="row">
+				          	<div class="col-md-4 col-sm-4 col-xs-12 animated fadeInLeftBig">	
+				          		<a href="<?php echo base_url('uploads/gallery/full/img01.jpg') ?>" class="gallery-images">
+				          			<div class="photo-gallery" style="background: url(<?php echo base_url('uploads/gallery/full/img01.jpg') ?>);"></div>
+				          		</a>
+				          	</div>
+				          	<div class="col-md-4 col-sm-4 col-xs-12 animated fadeInDownBig">
+				          		<a href="<?php echo base_url('uploads/gallery/full/img02.jpg') ?>" class="gallery-images">
+				          			<div class="photo-gallery" style="background: url(<?php echo base_url('uploads/gallery/full/img02.jpg') ?>);"></div>
+				          		</a>
+				          	</div>
+				          	<div class="col-md-4 col-sm-4 col-xs-12 animated fadeInRightBig">
+				          		<a href="<?php echo base_url('uploads/gallery/full/img03.jpg') ?>" class="gallery-images">
+				          			<div class="photo-gallery" style="background: url(<?php echo base_url('uploads/gallery/full/img03.jpg') ?>);"></div>
+				          		</a>
+				          	</div>
+				          </div>
+				        </div>
+				        <div class="item active">
+				          <div class="row">
+				          	<div class="col-md-4 col-sm-4 col-xs-12 animated fadeInRightBig">	
+				          		<a href="<?php echo base_url('uploads/gallery/full/img04.jpg') ?>" class="gallery-images">
+				          			<div class="photo-gallery" style="background: url(<?php echo base_url('uploads/gallery/full/img04.jpg') ?>);"></div>
+				          		</a>
+				          	</div>
+				          	<div class="col-md-4 col-sm-4 col-xs-12 animated fadeInUpBig">
+				          		<a href="<?php echo base_url('uploads/gallery/full/img05.jpg') ?>" class="gallery-images">
+				          			<div class="photo-gallery" style="background: url(<?php echo base_url('uploads/gallery/full/img05.jpg') ?>);"></div>
+				          		</a>
+				          	</div>
+				          	<div class="col-md-4 col-sm-4 col-xs-12 animated fadeInLeftBig">
+				          		<a href="<?php echo base_url('uploads/gallery/full/img06.jpg') ?>" class="gallery-images">
+				          			<div class="photo-gallery" style="background: url(<?php echo base_url('uploads/gallery/full/img06.jpg') ?>);"></div>
+				          		</a>
+				          	</div>
+				          </div>
+				        </div>
+				        <div class="item">
+				          <div class="row">
+				          	<div class="col-md-4 col-sm-4 col-xs-12 animated fadeInLeftBig">	
+				          		<a href="<?php echo base_url('uploads/gallery/full/img07.jpg') ?>" class="gallery-images">
+				          			<div class="photo-gallery" style="background: url(<?php echo base_url('uploads/gallery/full/img07.jpg') ?>);"></div>
+				          		</a>
+				          	</div>
+				          	<div class="col-md-4 col-sm-4 col-xs-12 animated fadeInDownBig">
+				          		<a href="<?php echo base_url('uploads/gallery/full/img08.jpg') ?>" class="gallery-images">
+				          			<div class="photo-gallery" style="background: url(<?php echo base_url('uploads/gallery/full/img08.jpg') ?>);"></div>
+				          		</a>
+				          	</div>
+				          	<div class="col-md-4 col-sm-4 col-xs-12 animated fadeInRightBig">
+				          		<a href="<?php echo base_url('uploads/gallery/full/img09.jpg') ?>" class="gallery-images">
+				          			<div class="photo-gallery" style="background: url(<?php echo base_url('uploads/gallery/full/img09.jpg') ?>);"></div>
+				          		</a>
+				          	</div>
+				          </div>
+				        </div>
+				       
+				      </div>
+				      <a class="left carousel-control" href="#wedding-photo" data-slide="prev">
+				        <span class="glyphicon glyphicon-chevron-left"></span>
+				      </a>
+				      <a class="right carousel-control" href="#wedding-photo" data-slide="next">
+				        <span class="glyphicon glyphicon-chevron-right"></span>
+				      </a>
+				    </div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end:photos -->
+
+	<!-- begin:story -->
+	<section id="story">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>The Story</h2>
+					<ul class="content">
+						<li>
+							<h3 class="content-avatar avatar-top">
+								<div class="img-avatar" style="background: url(<?php echo base_url('uploads/gallery/full/ho_yan2.jpg') ?>);"></div>
+							</h3>
+							<ul>
+								<!-- begin:content-text -->
+								<li class="content-item content-text">
+									<h3>Jack :"></h3>
+									<div class="text">I meet you at the first time. I smile, and you don't smile back to me. I was silent, and you laugh out loud alone. It's so romantic epic. :))</div>
+									<time datetime=""><i class="fa fa-calendar"></i> December, 19 2004</time>
+								</li>
+								<!-- end:content-text -->
+
+								<!-- begin:content-photo -->
+								<li class="content-item content-photo">
+									<img src="<?php echo base_url('uploads/gallery/full/romantic_2.jpg') ?>" alt="ditinggal rabi - together">	
+									<p class="caption">At the first date, we ate ice cream together. We enjoyed the evening. Sitting on the Waja along the way back to home. Telling each other ....</p>
+									<time datetime=""><i class="fa fa-calendar"></i> January, 19 2005</time>
+								</li>
+								<!-- end:content-photo -->
+
+								<!-- begin:content-photo -->
+								<li class="content-item content-photo">
+									<img src="<?php echo base_url('uploads/gallery/full/romantic_1.jpg') ?>" alt="ditinggal rabi - sleep together">
+									<time datetime=""><i class="fa fa-calendar"></i> September, 28 2014</time>	
+								</li>
+								<!-- end:content-photo -->
+
+								<!-- begin:content-chat -->
+								<li class="content-item content-chat">
+									<p class="text chat odd">A : Sist..</p>
+									<p class="text chat even">B : Yeah..</p>
+									<p class="text chat odd">A : Sist..</p>
+									<p class="text chat even">B : Yeahh broth..</p>
+									<p class="text chat odd">A : Would you be..</p>
+									And then silence....
+									<time datetime=""><i class="fa fa-calendar"></i> January, 26 2005</time>
+								</li>
+								<!-- end:content-chat -->
+
+								<!-- begin:content-quote -->
+								<li class="content-item content-quote">
+									<blockquote>
+										<p>I do not promise anything, I can only promise one thing. I would still handsome until tomorrow... <small>Jack Lee</small></p>
+										<time datetime=""><i class="fa fa-calendar"></i> March, 26 2014</time>
+									</blockquote>
+								</li>
+								<!-- end:content-quote -->
+
+								<!-- begin:content-photo -->
+								<li class="content-item content-photo">
+									<img src="<?php echo theme_img('img07.jpg') ?>" alt="ditinggal rabi - wedding ring">
+									<p class="caption">And finally, we are now getting married. :')</p>
+									<time datetime=""><i class="fa fa-calendar"></i> September, 09 2015</time>
+								</li>
+								<!-- end:content-photo -->
+
+								
+							</ul>
+						</li>
+
+						<li class="content-end">
+							<h3 class="content-avatar avatar-end">
+								<div class="img-avatar" style="background: url(<?php echo base_url('uploads/gallery/full/jack.jpg') ?>);"></div>
+							</h3>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end:story -->
+	
+	<!-- begin:contact -->
+	<section id="contact">
+		<div id="maps"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Contact Us</h2>
+					<div class="contact-container">
+						<div class="row">
+							<form>
+							<div class="col-md-6 col-sm-6">
+								<textarea rows="7" class="form-control no-resize" placeholder="Message"></textarea>
+							</div>
+							<!-- break -->
+
+							<div class="col-md-6 col-sm-6">
+								<input type="text" class="form-control" placeholder="Name">
+								<input type="email" class="form-control" placeholder="Email">
+								<input type="submit" value="Submit" class="btn btn-lg btn-green" />
+							</div>
+							</form>
+						</div>
+					</div>
+					<h5><span>~ Thank you ~</span></h5>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end:contact -->
+
+	<!-- begin:copyright -->
+	<section id="copyright">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Jack Lee & Chloe Fong</h2>
+					<h3>Yang sangat berbahagia</h3>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-12">
+					<ul class="list-inline social-icon">
+						<!--li><a href="#" class="icon-twitter" rel="tooltip" title="Twitter" data-placement="top"><i class="fa fa-twitter"></i></a></li-->
+						<li><a href="https://www.facebook.com/waichoon.lee/" target="_blank" class="icon-facebook" rel="tooltip" title="Facebook" data-placement="top"><i class="fa fa-facebook-square"></i></a></li>
+						<!--li><a href="#" class="icon-google" rel="tooltip" title="Google Plus" data-placement="top"><i class="fa fa-google-plus"></i></a></li>
+						<li><a href="#" class="icon-instagram" rel="tooltip" title="Instagram" data-placement="top"><i class="fa fa-instagram"></i></a></li-->
+					</ul>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-12">
+					<p>Jack Lee & Chloe Fong's Wedding Invitation built with <i class="fa fa-heart-o"></i> by <a href="https://www.facebook.com/waichoon.lee/" target="_blank">@Jack Lee</a></p>
+					<p>Copyright &copy; 2015 All Right Reserved.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end:copyright -->
